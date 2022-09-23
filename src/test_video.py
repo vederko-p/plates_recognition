9c152d4057b03b86370755157be326641db0dfc3
Максим Скибин <skibin-mvp@mail.ru>
2022-09-23 18:18:35 +0300

    skip frame property added

--- src/test_video.py
+++ src/test_video.py
@@ -1,29 +1,53 @@
 
 import cv2
 
+from models.fake_model import FakeModel
+
 from utils import check_source
 from configs.config_test_video import test_video_config
 
 
-def process_video(cap: cv2.VideoCapture, display: bool = False):
+def process_video(
+        cap: cv2.VideoCapture,
+        model,
+        display: bool = False,
+        skip_frame_by_model: int = 0
+) -> None:
+    skip_frame_by_model += 1
+    frames_counter = 0
     while cap.isOpened():
+        # get frame
         ret, frame = cap.read()
+        frames_counter += 1
+        result = None
+
         if ret:
+            # display
             if display:
                 cv2.imshow('Frame', frame)
+
+            # model
+            if not frames_counter % skip_frame_by_model:
+                result = model(frame)
+
+            # process result
+            print(result)
+
+            # quit
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
         else:
             break
-    pass
 
 
-def main(config: dict):
+def main(config: dict, model):
     v_cap = cv2.VideoCapture(config['video_filepath'])
     check_source(v_cap)
-    process_video(v_cap, config['display_video'])
-    pass
+    process_video(v_cap, model,
+                  display=config['display_video'],
+                  skip_frame_by_model=config['skip_frames_by_model'])
 
 
 if __name__ == '__main__':
-    main(test_video_config)
+    my_model = FakeModel()
+    main(test_video_config, my_model)
