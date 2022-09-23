b85e21122bd4dd55c20e33439a62c154b31f3cbb
Максим Скибин <skibin-mvp@mail.ru>
2022-09-23 23:01:25 +0300

    csv container for recognition results

---
+++ src/csv_container.py
@@ -0,0 +1,45 @@
+
+import os
+import time
+from typing import List
+
+
+def list_of_dicts_2_list_of_strings(lst: List[dict]) -> List[str]:
+    """Make list of strings from list of dicts with ordered keys.
+    Assuming that keys are equal in all dicts. Otherwise, the error will be
+    occurred or some keys might be lost."""
+    columns = sorted(lst[0].keys())
+    return [','.join([str(dct[k]) for k in columns]) + '\n' for dct in lst]
+
+
+class ContainerCSV:
+    def __init__(self, out_dir_path: str):
+        current_time = time.strftime('%Y-%M-%d_%H-%m-%S')
+        self.filename = f'output_{current_time}'
+        self.out_dir_path = out_dir_path
+        self.storage = []  # List of dicts.
+                           # Assuming that keys are equal in all dicts.
+
+    def save(self):
+        lines = list_of_dicts_2_list_of_strings(self.storage)
+        filepath = os.path.join(self.out_dir_path, self.filename) + '.csv'
+        with open(filepath, 'w') as output_file:
+            output_file.writelines(lines)
+        print(f'Data has been saved into .csv file: {filepath}')
+
+    def write_line(self, line: dict):
+        self.storage.append(line)
+
+
+if __name__ == '__main__':
+    csv = ContainerCSV('../csv_output')
+
+    import random
+    test_storage = [{k: random.randint(1, 100) for k in 'abcde'}
+                    for _ in range(10)]
+    for dct in test_storage:
+        print([v for v in dct.values()])
+        csv.write_line(dct)
+
+    csv.storage = test_storage
+    csv.save()
