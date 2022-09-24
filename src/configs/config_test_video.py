959194d4699d8efdab058e1f4d86accfd1aab640
Максим Скибин <skibin-mvp@mail.ru>
2022-09-24 13:07:57 +0300

    gt and st filepaths added

--- src/configs/config_test_video.py
+++ src/configs/config_test_video.py
@@ -1,6 +1,8 @@
 
 test_video_config = {
     'video_filepath': '../data/video/itmo_3_cfr_720.mp4',
+    'gt_filepath': '../data/gt for video/itmo_3.csv',
+    'st_filepath': None,
     'output_csv_directory_path': '../csv_output',
     'display_video': True,
     'skip_frames_by_model': 0
