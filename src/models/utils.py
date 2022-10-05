921d0374ffaba85f31ed2526b5135af8bec2658f
Максим Скибин <skibin-mvp@mail.ru>
2022-10-05 15:01:52 +0300

    3 functions added

--- src/models/utils.py
+++ src/models/utils.py
@@ -1,6 +1,7 @@
 
 from typing import Tuple
 
+import numpy as np
 import torch
 from torch import nn
 
@@ -16,3 +17,18 @@ def get_avg_patches(image: torch.tensor,
 def rgb01_2_hex(color: Tuple[float, float, float]):
     hex_values = map(lambda x: hex(int(x*255))[-2:], color)
     return '#' + ''.join(hex_values)
+
+
+def get_crop_from_np(image: np.array, xmin, ymin, xmax, ymax) -> np.array:
+    return image[int(ymin): int(ymax), int(xmin): int(xmax)]
+
+
+def np2tensor(
+    array: np.array, dims=(2, 0, 1),
+    brightness_normalize_coeff=255
+) -> torch.tensor:
+    return torch.tensor(array).permute(*dims) / brightness_normalize_coeff
+
+
+def tensor2np(tensor: torch.tensor, dims=(1, 2, 0)) -> np.array:
+    return tensor.permute(dims).numpy()
